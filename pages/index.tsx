87d9cc900ae8b4383731ac8af065589f237c0055
tienvv1234 <tienvv1234@gmail.com>
2022-12-07 11:17:32 +0700

    ISR implement

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -70,11 +72,36 @@ export default function Home() {
   )
 }
 
-export async function getServerSideProps() {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  return {
-    props: {
-
-    }
+export const getStaticPaths = async () => {
+  console.log('getStaticPaths')
+  const router = useRouter();
+  const response: any = await new Promise((resolve) => resolve({ data: [{id: 1}, {id: 2}] }));
+  if(router.isFallback) {
+    return <div>loading ...</div>
   }
+
+  const paths = response.data.map((item: any) => ({
+    params: { id: item.id },
+  }));
+
+  return { paths, fallback: true }; // blocking, true, false
+  // fallback: true - means that if the page is not pre-rendered, Next.js will attempt to generate the page on-demand. This approach is called Incremental Static Regeneration.
+  // fallback: false - means that if the page is not pre-rendered, Next.js will return a 404 page.
+  // fallback: 'blocking' - means that if the page is not pre-rendered, Next.js will wait for the page to be generated before returning the response. This approach is called Incremental Static Regeneration.
+}
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  console.log('getStaticProps');
+  const { params } = context;
+  console.log('params', params)
+  const response: any = await new Promise((resolve) => resolve({ data: [{id: 1}, {id: 2}] }));
+  console.log('response', response)
+  return (
+    props: {
+      data: 'response.data',
+    },
+    revalidate: 5, // In seconds re-generate the page (if the page is not pre-rendered, 
+    //Next.js will wait for the page to be generated before returning the response. This approach is called Incremental Static Regeneration.
+    // call after 5 seconds to get new data)
+  )
 }
