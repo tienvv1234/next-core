1c1124f856f791da7203012639bf66716469ea05
tienvv1234 <tienvv1234@gmail.com>
2022-12-08 17:08:57 +0700

    set up theme and material component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,48 @@
-import '../styles/globals.css'
-import React from 'react'
-import { AppPropsWithLayout } from '../models'
-import { EmptyLayout } from '../components/layout'
-import { SWRConfig } from 'swr'
-import axiosClient from '../api-client/axios-client'
+import React from 'react';
+import { SWRConfig } from 'swr';
+import axiosClient from '../api-client/axios-client';
+import { EmptyLayout } from '../components/layout';
+import { AppPropsWithLayout } from '../models';
+import '../styles/globals.css';
+
+import { CacheProvider } from '@emotion/react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { createEmotionCache, theme } from '../utils/';
+
+// client-side cache, shared for the whole session of the user in the browser
+const clientSideEmotionCache = createEmotionCache();
 
 // app cua nextjs dùng để init khởi tạo cái trang
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  console.log('App rerender')
-  React.useEffect(() => {
-    console.log('app mounting')
+export default function App({
+    Component,
+    pageProps,
+    emotionCache = clientSideEmotionCache,
+}: AppPropsWithLayout) {
+    console.log('App rerender');
+    React.useEffect(() => {
+        console.log('app mounting');
 
-    return () => console.log('app unmounting')
-  }, [])
+        return () => console.log('app unmounting');
+    }, []);
 
-  const Layout = Component.Layout || EmptyLayout
-  return (
-    <SWRConfig value={{
-      fetcher: url => axiosClient.get(url),// use axis 
-      shouldRetryOnError: false, /// retry when error default is true
-      }}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SWRConfig>
-  )
+    const Layout = Component.Layout || EmptyLayout;
+    return (
+        <CacheProvider value={emotionCache}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline>
+                    <SWRConfig
+                        value={{
+                            fetcher: (url) => axiosClient.get(url), // use axis
+                            shouldRetryOnError: false, /// retry when error default is true
+                        }}
+                    >
+                        <Layout>
+                            <Component {...pageProps} />
+                        </Layout>
+                    </SWRConfig>
+                </CssBaseline>
+            </ThemeProvider>
+        </CacheProvider>
+    );
 }
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
 import { AdminLayout, MainLayout } from '../components/layout';
+import { Box, Typography } from '@mui/material';
+export interface IAboutProps {}
 
-export interface IAboutProps {
-}
-
-export default function About (props: IAboutProps) {
-  return (
-    <>
-      <h1>About Page</h1>
-    </>
-  );
+export default function About(props: IAboutProps) {
+    return (
+        <Box>
+            <Typography component='h1' variant='h3' color="primary.main">About Page</Typography>
+        </Box>
+    );
 }
 
 About.Layout = AdminLayout;
