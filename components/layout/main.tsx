0308412742a9e70fd52f26beac2e00c2cbacb9fe
tienvv1234 <tienvv1234@gmail.com>
2022-12-12 15:00:56 +0700

    custom props and overrident style with theme material UI

--- components/layout/main.tsx
+++ components/layout/main.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { LayoutProps } from '../../models';
 import { Box, Stack } from '@mui/material';
 import { Header, Footer } from '../common/';
+import { Container } from '@mui/system';
 export interface IMainLayoutProps {}
 
 export function MainLayout({ children }: LayoutProps) {
@@ -16,6 +17,8 @@ export function MainLayout({ children }: LayoutProps) {
         <Stack minHeight='100vh'>
             <Header />
                 <Box component='main' flexGrow={1}>
+                    <Container maxWidth='sm' sx={{bgcolor: 'primary.main'}}>SM container</Container>
+                    <Container maxWidth='md' sx={{bgcolor: 'primary.main'}}>MD container</Container>
                     <Link href='/'>
                         <a>Home</a>
                     </Link>
