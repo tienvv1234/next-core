42cc2d0e695bf926df47c060632efa1de7cad28c
tienvv1234 <tienvv1234@gmail.com>
2022-12-14 17:29:55 +0700

    footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,9 +1,27 @@
 import * as React from 'react';
-import { Box } from '@mui/material';
+import { Box, Icon, Stack, Typography } from '@mui/material';
+import { Facebook } from '@mui/icons-material';
 export interface IFooterProps {}
 
 export function Footer(props: IFooterProps) {
     console.log('render Footer');
-
-    return <Box component='footer' py={2} textAlign='center'>Footer</Box>;
+    const socialLinks = [
+        { icon: Facebook, url: '' },
+        { icon: Facebook, url: '' },
+        { icon: Facebook, url: '' },
+        { icon: Facebook, url: '' },
+    ];
+    return <Box component='footer' py={2} textAlign='center'>
+        <Stack direction='row' justifyContent='center'>
+            {socialLinks.map((link, i) => (
+                <Box key={i} component='a' p={2} href={link.url} target='_blank' rel='noopener noreferrer'>
+                    <Icon component={link.icon} sx={{ fontSize: 48}} />
+                </Box>
+            ))}
+        </Stack>
+        <Typography>
+            Copyright @{new Date().getFullYear()} - All rights reserved
+        </Typography>
+    </Box>;
 }
+//alway use noopener noreferrer - security feature to prevent malicious code from running on your site
